db373efb0dc08c6c422def8617992351b7ae2474
Anørak <seb@iamseb.dev>
2023-05-22 18:56:07 +0200

    Added some comments on the modal component to help understand how it works

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,28 +7,34 @@ import './Modal.css';
 const Modal = (props) => {
 	const navigate = useNavigate();
 
+	// We store the props in variables
 	const { apiResponse, modal__display, redirectPath } = props;
 
+	// We can use the variables to style the modal
 	const modalStyle = {
 		modal__display,
 		redirectPath,
 	};
 
+	// We handle the click event to redirect the user to the path
 	const handleClick = () => {
 		navigate(redirectPath);
 	};
 
 	return (
+		// We use the variables to style the modal
 		<div className='modal__display' style={modalStyle.modal__display}>
 			<div className='modal__background'>
 				Modal
 				<div className='modal__container'>
 					<div className='modal__header'>
+						{/* We define the title based on the apiResponse (code) */}
 						<h2 className='modal__title'>
 							{apiResponse.status === 200 || apiResponse.status === 201
 								? 'Congratulations'
 								: 'Something went wrong'}
 						</h2>
+						{/* We handle the click on the close button to close the modal and redirect the user to the path */}
 						<button
 							className='modal__closeButton'
 							onClick={() => {
@@ -38,6 +44,7 @@ const Modal = (props) => {
 							X
 						</button>
 					</div>
+					{/* We display the message based on the apiResponse (message) */}
 					<div className='modal__body'>
 						<p>{apiResponse.statusText}</p>
 					</div>
